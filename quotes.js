fd8138888f53142c265f096bf9a463a9df3642d6
Jaron Maene <jaron.maene@gmail.com>
2025-09-30 14:55:15 +0200

    random quotes

---
+++ quotes.js
@@ -0,0 +1,26 @@
+const quotes = [
+  {
+    "text": "The preceding merely defines a way of thinking. But the point is to live.",
+    "author": "Albert Camus, Le Mythe de Sisyphe"
+  },
+  {
+    "text": "The map is not the territory.",
+    "author": "Alfred Korzybski"
+  },
+  {
+    "text": "For now, what is important is not finding the answer, but looking for it.",
+    "author": " Douglas Hofstadter, Gödel, Escher, Bach"
+  },
+  {
+    "text": "Yet here poor fool for all my lore, I stand no wiser than before.",
+    "author": "Goethe, Faust"
+  },
+  {
+    "text": "Man is fond of counting his troubles, but he does not count his joys.",
+    "author": "Fyodor Dostoevsky, Notes from the Underground"
+  }
+  {
+    "text": "All we have to decide is what to do with the time that is given to us.",
+    "author": "J.R.R. Tolkien, The Fellowship of the Ring"
+  }
+];
